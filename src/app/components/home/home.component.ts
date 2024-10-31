2d4bb0d69313409e3398ff1395634284165520cd
Milad Raeisi <miladsoft@yahoo.com>
2024-10-31 13:35:00 +0400

    Change the home UI

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,36 +1,37 @@
-import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, ChangeDetectorRef, Component, ViewEncapsulation } from '@angular/core';
-import { MatButtonModule } from '@angular/material/button';
+import { Component, OnDestroy, OnInit, ViewEncapsulation } from '@angular/core';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 import { RouterLink } from '@angular/router';
-import { SubscriptionService } from 'app/services/subscription.service';
-import { Filter, NostrEvent } from 'nostr-tools';
-import { ParseContentService } from 'app/services/parse-content.service';
+  import { Subject, takeUntil } from 'rxjs';
 
 @Component({
-  selector: 'landing-home',
-  templateUrl: './home.component.html',
-  encapsulation: ViewEncapsulation.None,
-  standalone: true,
-  imports: [MatButtonModule, RouterLink, MatIconModule, CommonModule],
-  changeDetection: ChangeDetectionStrategy.OnPush
+    selector: 'help-center',
+    templateUrl: './home.component.html',
+    encapsulation: ViewEncapsulation.None,
+    standalone: true,
+    imports: [
+        MatFormFieldModule,
+        MatInputModule,
+        MatIconModule,
+        RouterLink,
+        MatExpansionModule,
+    ],
 })
-export class LandingHomeComponent {
-  public events: NostrEvent[] = [];
-  public replay: NostrEvent[] = [];
+export class LandingHomeComponent implements OnInit, OnDestroy {
 
-  constructor(
-    private subscriptionService: SubscriptionService,
-    private parseContent: ParseContentService,
-    private _changeDetectorRefef: ChangeDetectorRef
-  ) {}
+    ngOnInit(): void {
 
-  ngOnInit(): void {
-  }
-
-  trackById(index: number, item: NostrEvent): string {
-    return item.id;
-  }
+    }
 
 
+    ngOnDestroy(): void {
+
+    }
+
+
+    trackByFn(index: number, item: any): any {
+        return item.id || index;
+    }
 }
