f9d1adf9d1678e68fdd32930e7abb6849a48f036
Milad Raeisi <miladsoft@yahoo.com>
2024-11-23 15:59:34 +0400

    Add remove password section

--- src/app/components/settings/security/security.component.ts
+++ src/app/components/settings/security/security.component.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import {
     ChangeDetectionStrategy,
+    ChangeDetectorRef,
     Component,
     OnInit,
     ViewEncapsulation,
@@ -32,79 +33,129 @@ import { SignerService } from 'app/services/signer.service';
         MatFormFieldModule,
         MatIconModule,
         MatInputModule,
-        MatSlideToggleModule,
         MatButtonModule,
         CommonModule
     ]
 })
 export class SettingsSecurityComponent implements OnInit {
-    securityForm: UntypedFormGroup;
+    changePasswordForm: UntypedFormGroup;
+    removePasswordForm: UntypedFormGroup;
 
+    isPasswordEnabled: boolean;
 
     constructor(
         private _formBuilder: UntypedFormBuilder,
         private _signerService: SignerService,
         private _snackBar: MatSnackBar,
-
+        private _changeDetectorRef: ChangeDetectorRef
     ) {}
 
     ngOnInit(): void {
-        const isPasswordEnabled = localStorage.getItem('usePassword') === 'true';
+        this.isPasswordEnabled = localStorage.getItem('usePassword') === 'true';
 
-        this.securityForm = this._formBuilder.group({
+        this.changePasswordForm = this._formBuilder.group({
             currentPassword: [
-                { value: '', disabled: !isPasswordEnabled },
-                isPasswordEnabled ? Validators.required : []
+                { value: '', disabled: !this.isPasswordEnabled },
+                this.isPasswordEnabled ? Validators.required : []
             ],
-            newPassword: ['', [Validators.required, Validators.minLength(3)]],
-            savePassword: [false],
+            newPassword: ['', [Validators.required]],
+            savePassword: [false]
         });
+
+        this.removePasswordForm = this._formBuilder.group({
+            currentPasswordRemove: ['', Validators.required]
+        });
+
+        this.updateFormState();
     }
 
-    openSnackBar(message: string, action: string = 'dismiss'): void {
-        this._snackBar.open(message, action, { duration: 3000 });
+    private updateFormState(): void {
+        if (this.isPasswordEnabled) {
+            this.changePasswordForm.get('currentPassword')?.enable();
+        } else {
+            this.changePasswordForm.get('currentPassword')?.disable();
+            this.changePasswordForm.get('currentPassword')?.clearValidators();
+        }
+
+        this._changeDetectorRef.detectChanges();
     }
 
     async changePassword(): Promise<void> {
-        if (this.securityForm.invalid) {
+        if (this.changePasswordForm.invalid) {
+            this.openSnackBar('Please fill out all required fields.');
             return;
         }
 
-        const currentPassword = this.securityForm.get('currentPassword')?.value;
-        const newPassword = this.securityForm.get('newPassword')?.value;
-        const savePassword = this.securityForm.get('savePassword')?.value;
+        const currentPassword = this.changePasswordForm.get('currentPassword')?.value;
+        const newPassword = this.changePasswordForm.get('newPassword')?.value;
+        const savePassword = this.changePasswordForm.get('savePassword')?.value;
 
         try {
-            const isPasswordEnabled = localStorage.getItem('usePassword') === 'true';
+            let secretKey: string | null = null;
 
-            if (isPasswordEnabled) {
-                const secretKey = await this._signerService.getSecretKey(currentPassword);
+            if (this.isPasswordEnabled) {
+                secretKey = await this._signerService.getSecretKey(currentPassword);
                 if (!secretKey) {
                     this.openSnackBar('Current password is incorrect.');
                     return;
                 }
-
-                await this._signerService.setSecretKey(secretKey, newPassword);
             } else {
-                const secretKey = await this._signerService.getSecretKey();
+                secretKey = await this._signerService.getSecretKey();
                 if (!secretKey) {
                     alert('Private key not found.');
                     return;
                 }
-
-                await this._signerService.setSecretKey(secretKey, newPassword);
             }
+
+            await this._signerService.setSecretKey(secretKey, newPassword);
 
             if (savePassword) {
                 this._signerService.savePassword(newPassword, 60);
             }
 
             this.openSnackBar('Password successfully changed.');
+            this.isPasswordEnabled = true;
+
+            this.updateFormState();
+            this.changePasswordForm.reset();
+            this._changeDetectorRef.detectChanges();
         } catch (error) {
             console.error('Error during password change:', error);
             this.openSnackBar('Failed to change the password.');
         }
     }
 
+    async removePassword(): Promise<void> {
+        if (this.removePasswordForm.invalid) {
+            this.openSnackBar('Please enter your current password to proceed.');
+            return;
+        }
 
+        const currentPassword = this.removePasswordForm.get('currentPasswordRemove')?.value;
+
+        try {
+            const secretKey = await this._signerService.getSecretKey(currentPassword);
+            if (!secretKey) {
+                this.openSnackBar('Current password is incorrect.');
+                return;
+            }
+
+            await this._signerService.setSecretKey(secretKey);
+
+            localStorage.removeItem('usePassword');
+            this.openSnackBar('Password successfully removed.');
+            this.isPasswordEnabled = false;
+
+            this.updateFormState();
+            this.removePasswordForm.reset();
+            this._changeDetectorRef.detectChanges();
+        } catch (error) {
+            console.error('Error during password removal:', error);
+            this.openSnackBar('Failed to remove the password.');
+        }
+    }
+
+    openSnackBar(message: string, action: string = 'dismiss'): void {
+        this._snackBar.open(message, action, { duration: 3000 });
+    }
 }
